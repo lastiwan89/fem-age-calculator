9b00a0b51d020137c6bc339dbca56cbb1d1f4ee8
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-24 02:47:44 +0000

    recreate repo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      purple: "#854dff",
+      black: "#151515",
+      white: "#fff",
+      grey: {
+        light: "#dbdbdb",
+        smokey: "#716f6f",
+      },
+      red: {
+        light: "#ff5959",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
